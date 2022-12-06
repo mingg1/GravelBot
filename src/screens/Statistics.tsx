b448e92524822089f8eb01d3c3f6e7f5e8192cdb
Minji Choi <comg2843@gmail.com>
2022-12-06 13:26:21 +0200

    added statistics screen

--- src/screens/Statistics.tsx
+++ src/screens/Statistics.tsx
@@ -1,4 +1,31 @@
-import { Text } from 'native-base';
+import { Box, Container, Heading, HStack, Text } from 'native-base';
 
-const Statistics = () => <Text>Statistics</Text>;
+const Statistics = () => (
+  <Box width="85%" marginX="auto">
+    <Box my={4} px={6} py={4} borderRadius={12} backgroundColor="white">
+      <HStack justifyContent="space-between" alignItems="center" space={4}>
+        <Text fontSize="20%" color="green.800" fontWeight={600}>
+          1km²
+        </Text>
+        <Heading size="sm"> Average covered area per hour</Heading>
+      </HStack>
+    </Box>
+    <Box my={4} px={6} py={4} borderRadius={12} backgroundColor="white">
+      <HStack justifyContent="space-between" alignItems="center" space={4}>
+        <Text fontSize="20%" color="green.800" fontWeight={600}>
+          25%
+        </Text>
+        <Heading size="sm"> Saved Co2 emission</Heading>
+      </HStack>
+    </Box>
+    <Box my={4} px={6} py={4} borderRadius={12} backgroundColor="white">
+      <HStack justifyContent="space-between" alignItems="center" space={4}>
+        <Text fontSize="20%" color="green.800" fontWeight={600}>
+          30%
+        </Text>
+        <Heading size="sm">Recycled gravel</Heading>
+      </HStack>
+    </Box>
+  </Box>
+);
 export default Statistics;
