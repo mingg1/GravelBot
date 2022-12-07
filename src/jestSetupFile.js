835adfc7e956818a9bf643324eaca2f73b2483b5
Minji Choi <comg2843@gmail.com>
2022-12-07 02:22:58 +0200

    added test config

---
+++ src/jestSetupFile.js
@@ -0,0 +1,18 @@
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+// include this line for mocking react-native-gesture-handler
+import 'react-native-gesture-handler/jestSetup';
+
+// include this section and the NativeAnimatedHelper section for mocking react-native-reanimated
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
